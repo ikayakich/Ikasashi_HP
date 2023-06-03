568169373b3e65f125522e7cb2d9e130f24967cd
ikayakich <100406203+ikayakich@users.noreply.github.com>
2023-06-03 20:08:55 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ var move_stalin_func = function() {
     stalin.style.left = stalin_spz + 'px';
     stalin.style.top = stalin_spz_y + 'px';
 
-    if( stalin_spz >= screen.availWidth - 20 ) {
+    if( stalin_spz >= screen.availWidth - 300 ) {
         stalin_spz = 0;
         stalin_spz_y = 0;
     }
@@ -36,7 +36,7 @@ var move_func = function() {
     spz++
     suwa_op.style.right = spz + "px";
 
-    if( spz >= screen.availWidth - 20 ) {
+    if( spz >= screen.availWidth - 10 ) {
         spz = 0;
     }
 }
